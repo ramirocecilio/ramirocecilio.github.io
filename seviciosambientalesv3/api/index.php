8f8bcd323077d0943b517512a5de1603843522b7
ramirocecilio <71830076+ramirocecilio@users.noreply.github.com>
2020-11-01 22:26:05 -0500

    sumarry (requiered)

---
+++ seviciosambientalesv3/api/index.php
@@ -0,0 +1,22 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../config/database.php';
+
+// Instantiate app
+$app = AppFactory::create();
+
+// Add Error Handling Middleware
+$app->addErrorMiddleware(true, false, false);
+
+// Add route callbacks
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write('Hello World');
+    return $response;
+});
+
+// Run application
+$app->run();
